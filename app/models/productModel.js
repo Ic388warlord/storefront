c96f9bc5052bade5bcee6e28378bb85115e85f27
Matthew <Matirix@live.com>
2023-11-16 18:55:56 -0800

    shopping cart finished

---
+++ app/models/productModel.js
@@ -0,0 +1,12 @@
+class Product {
+    constructor(product_id, product_category, product_description, product_images, product_name, product_price) {
+        this.product_id = product_id;
+        this.product_category = product_category;
+        this.product_description = product_description;
+        this.product_images = product_images;
+        this.product_name = product_name;
+        this.product_price = product_price;
+    }
+}
+
+export default Product;
