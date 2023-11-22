34918f1d434ca19c0f187fb14021316db021a7a4
Gareth Ng <ngys.gareth@gmail.com>
2023-11-22 15:26:09 -0800

    added contact form

--- app/contacts/page.js
+++ app/contacts/page.js
@@ -1,15 +1,84 @@
 'use client';
-import React from 'react';
 
-// Define your Contact function
+import React, { useState } from 'react';
+
 const Contacts = () => {
+  // State to hold form data
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  // Handle form input changes
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  // Handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add your logic for handling form submission here
+    console.log('Form submitted:', formData);
+  };
+
   return (
-    <div>
-      <h1>Contact Page</h1>
-      {/* Add your contact form or other content here */}
+    <div className="flex items-center justify-center h-screen">
+      <div className="bg-white text-black p-8 max-w-xl w-full rounded shadow-md">
+        <h1 className="text-2xl font-bold mb-6">Contact Page</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label htmlFor="name" className="block text-lg font-large text-gray-600">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              className="mt-1 p-2 w-full border rounded-md"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-lg font-large text-gray-600">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="mt-1 p-2 w-full border rounded-md"
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label htmlFor="message" className="block text-lg font-large text-gray-600">
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              rows="8"  
+              className="mt-1 p-2 w-full border rounded-md"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-black text-white py-2 px-4 rounded-md hover:bg-gray-800"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
 
-// Export the Contact function
 export default Contacts;
