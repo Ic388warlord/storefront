2315b8efc8ae5f5cff90d1d887391b04ed538650
AlexanderLiu <alexanderliu2016@gmail.com>
2023-12-03 18:01:35 -0800

    fix chatbot scrolling

--- app/components/chatbot.js
+++ app/components/chatbot.js
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react'
+import React, { useRef } from 'react'
 import { useState, useEffect } from 'react';
 import { FaArrowRight, FaCommentDots, FaSpeakap } from 'react-icons/fa';
 import API from '../utils/api';
@@ -9,6 +9,8 @@ const ChatBot = () => {
     const [text, setText] = useState('');
     const [messages, setMessages] = useState([]);
 
+    const chatRef = useRef();
+
     const handleSendButton = async () => {
         setText('');
         setMessages(messages => [...messages, { text: text, from: 'user' }]);
@@ -16,12 +18,22 @@ const ChatBot = () => {
         setMessages(messages => [...messages, { text: data, from: 'bot' }]);
     }
 
+    useEffect(() => {
+        console.log(open);
+        if (!chatRef) return;
+        if (open && chatRef.current) {
+            const lastMessage = chatRef.current.lastChild;
+            if (lastMessage) {
+                lastMessage.scrollIntoView({ behavior: 'smooth' });
+            }
+        }
+    }, [open, messages]);
 
   return (
     <div className="fixed bottom-0 left-0 w-80 z-50">
         {/* When open */}
         { open ? (
-            <div className='flex flex-col'>
+            <div className='flex flex-col h-96'>
 
             <button
             onClick={() => setOpen(false)}
@@ -30,7 +42,7 @@ const ChatBot = () => {
             Chat
             </button>
             {/* Chat Box */}
-            <div className="flex-1 overflow-y-auto p-4 bg-white" style={{ height: 'calc(100vh - [height_of_input_section])' }}>
+            <div ref={chatRef} className="flex-1 overflow-y-auto p-4 bg-white" style={{ height: 'calc(100vh - [height_of_input_section])' }}>
     {       messages.map((message, index) => (
                     <div key={index} className={`flex  items-center ${message.from === 'bot' ? 'justify-start' : 'justify-end'}`}>
                         <div className={`bg-gray-300 p-3 m-2 rounded-lg ${message.from === 'bot' ? 'ml-0' : 'ml-auto bg-blue-300'}`}>
@@ -44,6 +56,7 @@ const ChatBot = () => {
             <section className='flex bg-white border-t-2'>            
             <input
                   type="text"
+                  value={text}
                   placeholder="Type your message..."
                   className="w-full p-2 rounded"
                   onChange={(e) => setText(e.target.value)}
